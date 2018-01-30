69abc279a4c0fdfbc22719ec924dfad6db4dfec4
Clay Tercek <clay.tercek@gmail.com>
2018-01-30 13:02:29 -0500

    eye animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function () {
-    $('.eyeContainer').followCursor();
+    $('.eyeWhite').followCursor();
 
     var currentMousePos = { x: -1, y: -1 };
     var l_eyePosition = $('#leftEye').offset();
@@ -14,8 +14,8 @@ $(document).ready(function () {
         var l_eyeCursorDistance = Math.sqrt(Math.pow(currentMousePos.x - l_eyePosition.left, 2) + Math.pow(currentMousePos.y - l_eyePosition.top, 2));
         var r_eyeCursorDistance = Math.sqrt(Math.pow(currentMousePos.x - r_eyePosition.left, 2) + Math.pow(currentMousePos.y - r_eyePosition.top, 2));
 
-        var l_eyeDisplacement = 100 * l_eyeCursorDistance / 1500;
-        var r_eyeDisplacement = 100 * r_eyeCursorDistance / 1500;
+        var l_eyeDisplacement = 100 * l_eyeCursorDistance / 1300;
+        var r_eyeDisplacement = 100 * r_eyeCursorDistance / 1300;
 
         if (l_eyeDisplacement <= 40) {
             $('#leftEye').css('margin-top', '-' + l_eyeDisplacement + '%');
@@ -24,7 +24,24 @@ $(document).ready(function () {
         if (r_eyeDisplacement <= 40) {
             $('#rightEye').css('margin-top', '-' + r_eyeDisplacement + '%');
         };
-
-
     });
+
+    function blink() {
+        $('#blinkleft')[0].beginElement();
+        $('#blinkright')[0].beginElement();
+    };
+
+    (function loop() {
+        var rand = Math.round(Math.random() * (6000 - 500)) + 500;
+        setTimeout(function () {
+            //alert('A');
+            blink();
+            loop();
+        }, rand);
+    }());
+
+
+
+
 });
+

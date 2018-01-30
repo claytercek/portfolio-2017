4929a71f4cc0ab475873ec609eea317c8d66f9b1
Clay Tercek <clay.tercek@gmail.com>
2018-01-30 13:27:30 -0500

    Mobile Friendly Eyes!

--- js/main.js
+++ js/main.js
@@ -14,14 +14,14 @@ $(document).ready(function () {
         var l_eyeCursorDistance = Math.sqrt(Math.pow(currentMousePos.x - l_eyePosition.left, 2) + Math.pow(currentMousePos.y - l_eyePosition.top, 2));
         var r_eyeCursorDistance = Math.sqrt(Math.pow(currentMousePos.x - r_eyePosition.left, 2) + Math.pow(currentMousePos.y - r_eyePosition.top, 2));
 
-        var l_eyeDisplacement = 100 * l_eyeCursorDistance / 1300;
-        var r_eyeDisplacement = 100 * r_eyeCursorDistance / 1300;
+        var l_eyeDisplacement = 100 * l_eyeCursorDistance / 1600;
+        var r_eyeDisplacement = 100 * r_eyeCursorDistance / 1600;
 
-        if (l_eyeDisplacement <= 40) {
+        if (l_eyeDisplacement <= 30) {
             $('#leftEye').css('margin-top', '-' + l_eyeDisplacement + '%');
         };
 
-        if (r_eyeDisplacement <= 40) {
+        if (r_eyeDisplacement <= 30) {
             $('#rightEye').css('margin-top', '-' + r_eyeDisplacement + '%');
         };
     });

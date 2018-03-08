530060d7708e1ad17c02ca7c8a0955a0ca013f29
claytercek <clay.tercek@gmail.com>
2018-03-08 18:00:18 -0500

    ✨ Added Other Pages

--- js/main.js
+++ js/main.js
@@ -1,68 +1,34 @@
-$(document).ready(function() {
-	$(".eyeWhite").followCursor();
+{
+	var menu = document.querySelector(".menu");
+	var header = document.querySelector("header");
+	var navLinks = document.querySelectorAll("nav a");
 
-	var currentMousePos = { x: -1, y: -1 };
-	var l_eyePosition = $("#leftEye").offset();
-	var r_eyePosition = $("#rightEye").offset();
+	menu.addEventListener("click", openCloseMenu, false);
 
-	var eyewidth = $("#leftEye").width();
+	var menuIsOpen = false;
 
-	//get center of eyes instead of top left position:
-	l_eyePosition.left = l_eyePosition.left + eyewidth / 2;
-	l_eyePosition.top = l_eyePosition.top + eyewidth / 2;
-	r_eyePosition.left = r_eyePosition.left + eyewidth / 2;
-	r_eyePosition.top = r_eyePosition.top + eyewidth / 2;
+	function openCloseMenu() {
+		if (!menuIsOpen) {
+			header.classList.add("mobileHeaderOpen");
+			menu.src = "images/clear_white.svg";
 
-	$(document).mousemove(function(event) {
-		currentMousePos.x = event.pageX;
-		currentMousePos.y = event.pageY;
+			let i = 1;
+			for (var link of navLinks) {
+				link.style.transitionDuration = "0.5s";
+				link.style.transitionDelay = i * 0.15 + "s";
+				link.classList.add("navAnimateIn");
 
-		var l_eyeCursorDistance = Math.sqrt(
-			Math.pow(currentMousePos.x - l_eyePosition.left, 2) +
-				Math.pow(currentMousePos.y - l_eyePosition.top, 2)
-		);
-		var r_eyeCursorDistance = Math.sqrt(
-			Math.pow(currentMousePos.x - r_eyePosition.left, 2) +
-				Math.pow(currentMousePos.y - r_eyePosition.top, 2)
-		);
-
-		var l_eyeDisplacement = l_eyeCursorDistance / 25;
-		var r_eyeDisplacement = r_eyeCursorDistance / 25;
-
-		if (l_eyeDisplacement <= 30) {
-			$("#leftEye").css("top", 50 - l_eyeDisplacement + "%");
+				i++;
+			}
+		} else {
+			header.classList.remove("mobileHeaderOpen");
+			menu.src = "images/menu_white.svg";
+			for (var link of navLinks) {
+				link.style.transitionDuration = "0s";
+				link.style.transitionDelay = ".9s";
+				link.classList.remove("navAnimateIn");
+			}
 		}
-
-		if (r_eyeDisplacement <= 30) {
-			$("#rightEye").css("margin-top", "-" + r_eyeDisplacement + "%");
-		}
-	});
-
-	function blink() {
-		var l_eyelid = document.getElementById("l_eyelid");
-		var r_eyelid = document.getElementById("r_eyelid");
-		$("#blinkleft").attr(
-			"values",
-			"M0,300V0h300v300H0z M150.5,90.5C95.6,90.5,44,111.8,5.2,150.6L4.8,151l0.4,0.4 c38.8,38.8,90.4,60.2,145.3,60.2c54.9,0,106.5-21.4,145.3-60.2l0.4-0.4l-0.4-0.4C257,111.8,205.4,90.5,150.5,90.5z  ; M0,300V0h300v300H0z M150.5,150c-54.9,0-106.5,0.4-145.3,1l-0.4,0l0.4,0c38.8,0.6,90.4,1,145.3,1 c54.9,0,106.5-0.4,145.3-1l0.4,0l-0.4,0C257,150.4,205.4,150,150.5,150z ; M0,300V0h300v300H0z M150.5,90.5C95.6,90.5,44,111.8,5.2,150.6L4.8,151l0.4,0.4 c38.8,38.8,90.4,60.2,145.3,60.2c54.9,0,106.5-21.4,145.3-60.2l0.4-0.4l-0.4-0.4C257,111.8,205.4,90.5,150.5,90.5z "
-		);
-		l_eyelid.pauseAnimations();
-		l_eyelid.setCurrentTime(0);
-		$("#blinkright").attr(
-			"values",
-			"M0,300V0h300v300H0z M150.5,90.5C95.6,90.5,44,111.8,5.2,150.6L4.8,151l0.4,0.4 c38.8,38.8,90.4,60.2,145.3,60.2c54.9,0,106.5-21.4,145.3-60.2l0.4-0.4l-0.4-0.4C257,111.8,205.4,90.5,150.5,90.5z  ; M0,300V0h300v300H0z M150.5,150c-54.9,0-106.5,0.4-145.3,1l-0.4,0l0.4,0c38.8,0.6,90.4,1,145.3,1 c54.9,0,106.5-0.4,145.3-1l0.4,0l-0.4,0C257,150.4,205.4,150,150.5,150z ; M0,300V0h300v300H0z M150.5,90.5C95.6,90.5,44,111.8,5.2,150.6L4.8,151l0.4,0.4 c38.8,38.8,90.4,60.2,145.3,60.2c54.9,0,106.5-21.4,145.3-60.2l0.4-0.4l-0.4-0.4C257,111.8,205.4,90.5,150.5,90.5z "
-		);
-		r_eyelid.pauseAnimations();
-		r_eyelid.setCurrentTime(0);
-
-		l_eyelid.unpauseAnimations();
-		r_eyelid.unpauseAnimations();
+		menuIsOpen = !menuIsOpen;
 	}
-
-	(function loop() {
-		var rand = Math.round(Math.random() * (6000 - 500)) + 500;
-		setTimeout(function() {
-			blink();
-			loop();
-		}, rand);
-	})();
-});
+}
